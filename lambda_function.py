584eb8577cd9809f2da5e85d0d073faa227acdea
Tanuj Jain <tanuj.jain@idealo.de>
2019-09-08 14:36:52 +0200

    Remove test functioanlity.

--- lambda_function.py
+++ lambda_function.py
@@ -3,25 +3,22 @@ import json
 from validation import validate_date
 from mail_sender import send_mail
 
-FILE_NAME = 'contact_info.csv'
+FILE_NAME = "contact_info.csv"
 
 
 def lambda_handler(event, context):
     eligible_persons = []
 
-    with open(FILE_NAME, 'r') as f:
+    with open(FILE_NAME, "r") as f:
         csv_dictreader = csv.DictReader(f)
         for r in csv_dictreader:
-            print(r.get('beginning_date'))
-            valid = validate_date(r.get('beginning_date'), r.get('Frequency'))
+            print(r.get("beginning_date"))
+            valid = validate_date(r.get("beginning_date"), r.get("Frequency"))
             if valid:
-                eligible_persons.append(r.get('Person'))
+                eligible_persons.append(r.get("Person"))
     print(eligible_persons)
 
-    eligible_persons = ','.join(eligible_persons)
+    eligible_persons = ",".join(eligible_persons)
     ret_val = send_mail(eligible_persons)
-    status_body = 'Successfully sent the mail' if ret_val else 'Some shit!'
-    return {
-        'statusCode': 200,
-        'body': json.dumps(status_body)
-    }
+    status_body = "Successfully sent the mail" if ret_val else "Some shit!"
+    return {"statusCode": 200, "body": json.dumps(status_body)}
--- validation.py
+++ validation.py
@@ -3,11 +3,13 @@ import datetime
 
 def validate_date(in_date, freq):
     freq = int(freq)  # Fixing type for frequency
-    format_str = '%d-%m-%Y'  # Desired datetime format
-    datetime_obj_in = datetime.datetime.strptime(in_date, format_str)  # Conversion of beginning_date to datetime
+    format_str = "%d-%m-%Y"  # Desired datetime format
+    datetime_obj_in = datetime.datetime.strptime(
+        in_date, format_str
+    )  # Conversion of beginning_date to datetime
     weeks_elapsed = int((datetime.datetime.now() - datetime_obj_in).days / 7)
     print(weeks_elapsed)
-    if (weeks_elapsed % freq == 0):  # and (weeks_elapsed!=0)): Uncomment if testing successful
+    if (weeks_elapsed % freq == 0) and (weeks_elapsed != 0):
         valid = 1
     else:
         valid = 0
